c13ce17aba40640f02c0cc0595bdb667d9f0baf6
rasangamadhushan <rmsrasanga@gmail.com>
2025-11-04 00:05:13 +0530

    Create MergeSort.java
    Efficient implementation of the Merge Sort algorithm in Java, including step counting and execution time measurement.

---
+++ MergeSort.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class MergeSort {
+    private static int steps;
+
+    Public static SortResult sort(int[] data) {
+        int[] arr = Arrays.copyOf(data, data.length);
+        steps = 0;
+        long start = System.nanoTime();
+        mergeSort(arr, left:0, arr.length - 1);
+        long end = System.nanoTime();
+        double duration = (end - start) / 1e6;
+        return new SortResult(arr, duration, steps);
+    }
+
+    private static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i <n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j <n2; j++)
+            R[j] = arr[mid + 1 + j];
+            
+        int i = 0, j = 0, k = left;
+
+        While (i < n1 && j < n2) {
+            steps++;
+            if  (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k++] = L[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = R[j++];
+        }
+    }
+}
